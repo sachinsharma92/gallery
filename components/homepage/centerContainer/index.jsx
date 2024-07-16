fefa878cb6135959e517cfcfb390495827a93946
sachinsharma92 <sachindesigner2018@gmail.com>
2024-07-16 15:40:49 +0530

    ADD: slider

--- components/homepage/centerContainer/index.jsx
+++ components/homepage/centerContainer/index.jsx
@@ -1,9 +1,15 @@
 import { ImageCustom } from "@/components/ui/imageCustom";
 import { IoIosExpand, IoMdClose } from "react-icons/io";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay"; // Import autoplay styles
+
+// Import required modules
+import { Autoplay } from "swiper/modules";
 
 const CenterContainer = ({ clickHandler, isFullScreen }) => {
   return (
-    <div className="relative">
+    <div className="relative min-h-full">
       <button
         className="w-8 sm:w-11 h-8 sm:h-11 bg-white border absolute right-2 sm:right-5 top-2 sm:top-5 z-10 flex justify-center items-center"
         onClick={clickHandler}
@@ -11,13 +17,56 @@ const CenterContainer = ({ clickHandler, isFullScreen }) => {
         {!isFullScreen ? <IoIosExpand size={20} /> : <IoMdClose size={20} />}
       </button>
 
-      <ImageCustom
-        src="/images/3d-bg.jpg"
-        width={2560}
-        height={1068}
-        alt="bannerImg"
-        className="center-image"
-      />
+      <div className="swiper-center-slider">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="mySwiper"
+          autoplay={{
+            delay: 3000,
+          }}
+          modules={[Autoplay]}
+        >
+          <SwiperSlide>
+            <ImageCustom
+              src="/images/3d-bg.jpg"
+              width={2560}
+              height={1068}
+              alt="bannerImg"
+              className="center-image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ImageCustom
+              src="/images/3d-bg.jpg"
+              width={2560}
+              height={1068}
+              alt="bannerImg"
+              className="center-image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ImageCustom
+              src="/images/3d-bg.jpg"
+              width={2560}
+              height={1068}
+              alt="bannerImg"
+              className="center-image"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <ImageCustom
+              src="/images/3d-bg.jpg"
+              width={2560}
+              height={1068}
+              alt="bannerImg"
+              className="center-image"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
